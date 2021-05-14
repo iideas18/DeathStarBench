3a4734694fc743c14743d7e5490b72c3590f35b4
Yan <1524285681@qq.com>
2021-05-14 02:55:30 -0400

    clear

--- socialNetwork/docker/openresty-thrift/lua-thrift/RpcClientFactory.lua
+++ socialNetwork/docker/openresty-thrift/lua-thrift/RpcClientFactory.lua
@@ -5,8 +5,7 @@
 local RpcClient = require "RpcClient"
 --local TestServiceClient = require "resty.thrift.thrift-idl.lua_test_TestService"
 local RpcClientFactory = RpcClient:new({
-	__type = 'Client',
-    timeout = 10000
+	__type = 'Client'
 })
 function RpcClientFactory:createClient(thriftClient, ip, port, timeout)
     local protocol = self:init(ip, port, timeout)
